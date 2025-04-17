d5e6f2ce226e4a7ba974bb650d7dae551271ecc5
moazzzmedhat <m.gafer2543@su.edu.eg>
2025-04-17 20:42:14 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# Guess My Number Game
+
+A fun and interactive number guessing game built with HTML, CSS, and JavaScript. Try to guess the secret number between 1 and 20!
+
+## Features
+
+- Simple and intuitive user interface
+- Score tracking
+- Highscore functionality
+- Responsive design
+
+## How to Play
+
+1. Enter your guess in the input field
+2. Click "Check!" to submit your guess
+3. The game will tell you if your guess is too high or too low
+4. Try to guess the number with as few attempts as possible to achieve a high score!
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Getting Started
+
+Simply open the `index.html` file in your web browser to start playing the game. 
---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let secert_number = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  {
+    const guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+      document.querySelector('.message').textContent = 'd5l raqam ya 3gl';
+    } else if (guess === secert_number) {
+      document.querySelector('.message').textContent = '3ash ya 3gl';
+
+      document.querySelector('.number').textContent = secert_number;
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    } else if (guess > secert_number) {
+      document.querySelector('.message').textContent = 'ta7t ya 3ars';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (guess < secert_number) {
+      document.querySelector('.message').textContent = 'a3la ya 3ars';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+    if (score == 0) {
+      document.querySelector('.message').textContent = '5srt ya 3ars';
+      score == 0;
+      document.querySelector('.score').textContent = score;
+    }
+
+    if (score < 0) {
+      document.querySelector('.message').textContent = '5srt ya 3ars';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secert_number = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'start guessing';
+});
